3cd397c96053710bcd33789f9ad3ac574b612f52
LukeChuang890212 <zhuang890212@gmail.com>
2025-02-10 23:26:13 +0800

    revise

--- EBMR_Methods.r
+++ EBMR_Methods.r
@@ -1,10 +1,11 @@
+library(Matrix)
 library(R6)
 
 MyClass <- R6Class("MyClass",
                    public = list(
                      # Public fields (variables)
                      h = NULL,
-                     candidate_ps = NULL,
+                     ps.matrix = NULL,
                      r = NULL,
                      init = NULL,
                      discrete_dim = 0,
@@ -29,55 +30,55 @@ MyClass <- R6Class("MyClass",
                    )
 )
 
-estimate_nu = function(h, candidate_ps, r, init){
-  x1 = h[[1]]; x2 = h[[2]];
-  if(!is.null(x1)) x1 = as.matrix(x1)
-  if(!is.null(x2)) x2 = as.matrix(x2)
-
-  N = length(r)
-  discrete_dim = 0
-  if(!is.null(x1)) for(j in 1:ncol(x1)) discrete_dim = discrete_dim + length(unique(x1[,j]))
-  discrete_dim = ifelse(discrete_dim == 0, 0, discrete_dim - ncol(x1) + 1) # to avoid collinearity
-  continuous_dim = ifelse(!is.null(x2), ncol(x2), 0)
-  h_dim = discrete_dim + continuous_dim
-
-  J = ncol(candidate_ps)
-  if(is.null(init)) init = rep(0, J)
-
+estimate_nu = function(ps.matrix, h_x, init = NULL){
+  result = separate_variable_types(h_x)
+  h_x1 = result$x1
+  h_x2 = result$x2
+  
+  J = ncol(ps.matrix)
+  
   d = NULL
-  if(!is.null(x1)){
-    x1 =  as.data.frame(x1)
-    for(j in 1:ncol(x1)) x1[, j] = as.factor(x1[, j])
-    d = model.matrix(lm(rep(1, N)~., data =  x1))
+  if(ncol(h_x1) > 0){
+    for(j in 1:ncol(h_x1)) h_x1[, j] = as.factor(h_x1[, j])
+    d = model.matrix(lm(rep(1, n)~., data =  h_x1))
   }
   
+  discrete_dim = ncol(d)
+  continuous_dim = ncol(h_x2)
+  h_dim = discrete_dim + continuous_dim
+  
+  r = as.matrix(r)
+  y = as.matrix(y)
+  x = as.matrix(x)
+  h_x1 = as.matrix(h_x1)
+  h_x2 = as.matrix(h_x2)
+  
   g = function(nu){
-    g.matrix = matrix(NA, h_dim, N)
-    rw = rep(0, N)
-    rw[r == 1] = 1/(candidate_ps%*%nu)
+    g.matrix = matrix(NA, n, h_dim)
+    rw = r/(ps.matrix%*%nu)
     if(discrete_dim > 0){
-      for(l in 1:discrete_dim){g.matrix[l,] = t(d)[l,]*(rw-1)}
+      for(l in 1:discrete_dim){g.matrix[, l] = d[, l]*(rw-1)}
     }
     if(continuous_dim > 0){
-      for(l in (discrete_dim+1):(discrete_dim+continuous_dim)) g.matrix[l,] = t(x2)[l-discrete_dim,]*(rw-1)
+      for(l in (discrete_dim+1):(discrete_dim+continuous_dim)) g.matrix[, l] = h_x2[, l-discrete_dim]*(rw-1)
     }
     return(g.matrix)
   }
 
   G = function(g.matrix){
-    return(matrix(apply(g.matrix, 1, mean), h_dim, 1))
+    return(matrix(apply(g.matrix, 2, mean), h_dim, 1))
   }
-
+  
   W = function(g.matrix){
-    return(solve(g.matrix%*%t(g.matrix)/N))
+    return(solve(t(g.matrix)%*%g.matrix/n))
   }
-
+  
   Gamma = function(nu){
-    gamma = array(NA, dim = c(h_dim, J, N))
-    for(i in 1:h_dim){
-      gamma[i,,] = t(jacobian(function(nu.v) g(nu.v)[i,], nu))
+    Gamma.arr = array(NA, dim = c(h_dim, n, J))
+    for(l in 1:h_dim){
+      Gamma.arr[l,,] = jacobian(function(nu) g(nu)[, l], nu)
     }
-    return(apply(gamma, c(1, 2), mean))
+    return(apply(Gamma.arr, c(1, 3), mean))
   }
 
   obj = function(nu){
@@ -86,14 +87,15 @@ estimate_nu = function(h, candidate_ps, r, init){
     value = t(G.hat)%*%G.hat
     return(ifelse(is.infinite(value) || is.na(value), 10^8, value))
   }
-
+  
+  if(is.null(init)) init = rep(0, J)
   nu_sol_path = matrix(init, J)
   conv_err = 10^8
   t = 1
 
   while (conv_err > 10^(-8) & t < 1000){
     opt = optim(nu_sol_path[, t], obj, method = "L-BFGS-B",
-                lower = rep(-Inf, ncol(candidate_ps)), upper = rep(Inf, ncol(candidate_ps)))
+                lower = rep(-Inf, ncol(ps.matrix)), upper = rep(Inf, ncol(ps.matrix)))
     nu_sol_path = cbind(nu_sol_path, opt$par)
     g.matrix = g(nu_sol_path[,t+1]); W.hat = W(g.matrix);
     obj = function(nu){
@@ -108,125 +110,126 @@ estimate_nu = function(h, candidate_ps, r, init){
 
   nu.hat = nu_sol_path[, t]
   Gamma.hat = Gamma(nu.hat)
-  g.matrix = g(nu.hat); W.hat = W(g);
-  S = var(t(g.matrix))
+  g.matrix = g(nu.hat)
+  W.hat = W(g.matrix)
+  S = var(g.matrix)
   # cov.hat = solve(t(Gamma.hat)%*%W.hat%*%Gamma.hat)/N
-  cov.hat = solve(t(Gamma.hat)%*%W.hat%*%Gamma.hat)%*%t(Gamma.hat)%*%W.hat%*%S%*%W.hat%*%Gamma.hat%*%solve(t(Gamma.hat)%*%W.hat%*%Gamma.hat)/N
-  K_nu = eta%*%solve(t(Gamma.hat)%*%W.hat%*%Gamma.hat)%*%t(Gamma.hat)%*%W.hat
+  cov.hat = solve(t(Gamma.hat)%*%W.hat%*%Gamma.hat)%*%t(Gamma.hat)%*%W.hat%*%S%*%W.hat%*%Gamma.hat%*%solve(t(Gamma.hat)%*%W.hat%*%Gamma.hat)/n
   se = sqrt(diag(cov.hat))
-
-  return(list(sol.path = nu_sol_path, nu.hat = nu.hat, cov.hat = cov.hat,
-              se = se, lower = nu.hat-qnorm(0.975)*se, upper = nu.hat+qnorm(0.975)*se,
-              g.matrix = g.matrix, K_nu = K_nu, h = t(cbind(d, x2))))
+  
+  results = list(coefficients = nu.hat, 
+                 sol.path = nu_sol_path, 
+                 cov.hat = cov.hat,
+                 se = se, 
+                 lower = nu.hat-qnorm(0.975)*se, 
+                 upper = nu.hat+qnorm(0.975)*se,
+                 g.matrix = g.matrix, 
+                 K = solve(t(Gamma.hat)%*%W.hat%*%Gamma.hat)%*%t(Gamma.hat)%*%W.hat,
+                 h_x = cbind(d, h_x2))
+  
+  return(results)
 }
 
-collect.propensity = function(pi.list, pi.fit.list, alpha.list){
+collect.propensity = function(ps_model.list, ps_fit.list, alpha.list){
   n = sum(r)
-  candidate_ps = NULL
+  ps.matrix = NULL
   #------------------------------------------------------------------------------#
   # Collect the propensity score models
   #------------------------------------------------------------------------------#
-  J = length(pi.list)
-  candidate_ps = matrix(NA, n, J)
+  J = length(ps_model.list)
+  ps.matrix = matrix(NA, n, J)
   for(j in 1:J){
-    candidate_ps[, j] = pi.list[[j]](y[r == 1], as.matrix(dat[r == 1, pi.fit.list[[j]]$model.x.names]), alpha.list[[j]], n)
+    ps.matrix[, j] = ps_model.list[[j]](y[r == 1], as.matrix(data[r == 1, ps_fit.list[[j]]$model.x.names]), alpha.list[[j]], n)
   }
   #------------------------------------------------------------------------------#
-  return(list(pi.list = pi.list, pi.fit.list = pi.fit.list, alpha.list = alpha.list, candidate_ps = candidate_ps))
+  return(list(ps_model.list = ps_model.list, ps_fit.list = ps_fit.list, alpha.list = alpha.list, ps.matrix = ps.matrix))
 }
 
-get.candidate_ps = function(pi.fit.list, pi.list, alpha.list, nonrespondent = TRUE){
-  J = length(pi.fit.list)
+get.ps.matrix = function(ps_fit.list, ps_model.list, alpha.list, nonrespondent = TRUE){
+  J = length(ps_fit.list)
   id = which(r == ifelse(nonrespondent, 0, 1))
   n = length(id)
-  candidate_ps = matrix(NA, n, J)
+  ps.matrix = matrix(NA, n, J)
   for(j in 1:J){
-    candidate_ps[, j] = pi.list[[j]](y[id],  as.matrix(dat[id, pi.fit.list[[j]]$model.x.names]), alpha.list[[j]], n)
+    ps.matrix[, j] = ps_model.list[[j]](y[id],  as.matrix(data[id, ps_fit.list[[j]]$model.x.names]), alpha.list[[j]], n)
   }
-  return(candidate_ps)
+  return(ps.matrix)
 }
 
-EBMR_IPW = function(pi.fit.list, h, family = "gaussian", ortho = FALSE, true.pi = NULL, perturb = rep(1, N)){
+EBMR_IPW = function(ps_fit.list, h_x, true_ps = NULL){
   ################################################################################
   # Collect the propensity score models
   ################################################################################
-  pi.list = lapply(pi.fit.list, function(pi.fit) pi.fit$model)
-  alpha.list = lapply(pi.fit.list, function(pi.fit) pi.fit$nu.hat)
-  propensity.collected = collect.propensity(pi.list, pi.fit.list, alpha.list)
-  pi.list = propensity.collected$pi.list
-  pi.fit.list = propensity.collected$pi.fit.list
-  alpha.list = propensity.collected$alpha.list
-  candidate_ps = propensity.collected$candidate_ps
-  J = length(pi.list)
+  J = length(ps_fit.list)
+  ps_model.list = lapply(ps_fit.list, function(ps_fit) ps_fit$model)
+  alpha.list = lapply(ps_fit.list, function(ps_fit) ps_fit$coefficients)
+  alpha_dim = unlist(lapply(alpha.list, length))
+  ps.matrix = do.call(cbind, lapply(ps_fit.list, function(ps_fit) ps_fit$fitted.values))
   ################################################################################
 
-  dim.alpha = unlist(lapply(alpha.list, length))
 
   ################################################################################
   # Compress the propensity score models
   ################################################################################
-  nu.fit = estimate_nu(h, candidate_ps, r, init = rep(1/J, J), perturb = perturb)
-  nu = nu.fit$nu.hat
-  nu = as.matrix((nu^2)/as.numeric(t(nu)%*%nu))
-  compressed.pi = rep(1, N)
-  compressed.pi[r == 1] = candidate_ps%*%nu
+  ensemble_fit = estimate_nu(ps.matrix, h_x, init = rep(1/J, J))
+  nu.hat = ensemble_fit$coefficients
+  w.hat = nu.hat^2/sum(nu.hat^2)
+  ensemble_ps = ps.matrix%*%w.hat
   ################################################################################
 
   ################################################################################
-  # calculate pi.x.alpha.m, beta.pi.prime ... (for estimating the ASE)
+  # calculate dot_pi, beta.pi.prime ... (for estimating the ASE)
   ################################################################################
-  dim.alpha = unlist(lapply(alpha.list, length))
-
-  imputed.candidate_ps = matrix(1, N, J)
-  imputed.candidate_ps[r == 1,] =  get.candidate_ps(pi.fit.list, pi.list, alpha.list, nonrespondent = FALSE)
-
-  imputed.pi = rep(1, N)
-  imputed.pi[r == 1] = imputed.candidate_ps[r == 1,]%*%nu
-
-  pi.x.alpha.m = matrix(0, N, sum(dim.alpha))
+  # imputed.ps.matrix = matrix(1, N, J)
+  # imputed.ps.matrix[r == 1,] =  get.ps.matrix(ps_fit.list, ps_model.list, alpha.list, nonrespondent = FALSE)
+  # 
+  # imputed.pi = rep(1, N)
+  # imputed.pi[r == 1] = imputed.ps.matrix[r == 1,]%*%nu
+    
+  dot_pi = matrix(NA, n, sum(alpha_dim))
   for(j in 1:J){
-    pi.x.alpha.m[r == 1, (sum(dim.alpha[0:(j-1)])+1):sum(dim.alpha[1:j])] = jacobian(function(alpha.v) pi.list[[j]](y[r == 1],  as.matrix(dat[r == 1, pi.fit.list[[j]]$model.x.names]), alpha.v, n), alpha.list[[j]])
+    x = as.matrix(data[ps_fit.list[[j]]$model.x.names])
+    dot_pi[, (sum(alpha_dim[0:(j-1)])+1):sum(alpha_dim[1:j])] = jacobian(function(alpha) ps_model.list[[j]](x, y, alpha), alpha.list[[j]])
   }
-  beta.pi.prime = -(nu.fit$K_nu%*%nu.fit$h%*%(-as.vector(r*((imputed.pi)^(-2)))*t(t(pi.x.alpha.m)*rep(nu, dim.alpha))))/N
-
-  norm.adj = function(w){
-    (diag(2*w)*sum(w^2)-2*(w^2)%*%t(w))/(sum(w^2)^2)
+  E_dot_g = -(t(dot_pi)*rep(w.hat, alpha_dim))%*%(ensemble_fit$h_x*as.vector(r*((ensemble_ps)^(-2))))/n
+  
+  dot_W = function(w){
+    (diag(2*w)*sum(w^2)-2*(w)%*%t(w^2))/(sum(w^2)^2)
   }
-
-  # norm.adj = function(w){
-  #   (diag(2*w)*sum(w^2)-2*(w)%*%t(w^2))/(sum(w^2)^2)
-  # }
-
-  norm.nu.adj = 0; if(length(nu) > 1) norm.nu.adj = norm.adj(nu.fit$nu.hat)
-
-  alpha.iid =  (c(lapply(pi.fit.list, function(pi.fit) -pi.fit$K)) %>% bdiag() %*%
-                  do.call(rbind, lapply(pi.fit.list, function(pi.fit) pi.fit$g)) %>% as.matrix())[1:sum(dim.alpha),]
-
-  beta.pi.iid = -nu.fit$K_nu%*%nu.fit$g
+  dot_W_nu_hat = 0; if(length(nu.hat) > 1) dot_W_nu_hat = dot_W(nu.hat)
+  
+  H_alpha.w = apply(t((t(dot_pi)*rep(w.hat, alpha_dim)))*as.vector(r*y*((ensemble_ps)^(-2))), 2, mean)
+  w.H_nu = apply(ps.matrix%*%t(dot_W_nu_hat)*as.vector(r*y*((ensemble_ps)^(-2))), 2, mean)
+  
+  K_alpha = c(lapply(ps_fit.list, function(ps_fit) ps_fit$K)) %>% bdiag()
+  g_all = do.call(rbind, lapply(ps_fit.list, function(ps_fit) t(ps_fit$g.matrix)))
+  K_nu = ensemble_fit$K
+  g = t(ensemble_fit$g.matrix)
+  
+  # alpha.iid = -K_alpha%*%g_all 
+  # 
+  # nu.iid = -ensemble_fit$K%*%t(ensemble_fit$g.matrix)
   ################################################################################
 
   ################################################################################
   # IPW estimator for the population mean mu
   ################################################################################
-  mu.IPW = mean(perturb*r/compressed.pi*y)
+  mu.IPW = mean(r/ensemble_ps*y)
   mu.IPW.true = NULL
-
-  if(!is.null(true.pi)){
-    mu.IPW.true = mean(r/true.pi*y)
+  if(!is.null(true_ps)){
+    mu.IPW.true = mean(r/true_ps*y)
   }
   ################################################################################
 
   ################################################################################
   # Estimate the asymptotic variance of IPW
   ################################################################################
-  ipw.alpha = apply(-r*y/(imputed.pi^2)*(t(t(pi.x.alpha.m)*rep(nu, dim.alpha))), 2, mean)
-  ipw.beta = apply(-r*y/(imputed.pi^2)*(imputed.candidate_ps%*%norm.nu.adj), 2, mean)
-  E.H = ipw.alpha+ipw.beta%*%beta.pi.prime
+  mu.IPW.iid = as.vector(t(r/ensemble_ps*y)
+                         +(t(H_alpha.w)+t(w.H_nu)%*%K_nu%*%t(E_dot_g))%*%K_alpha%*%g_all
+                         +t(w.H_nu)%*%K_nu%*%g)
+  se.IPW = sqrt(var(mu.IPW.iid)/n)
 
-  mu.IPW.iid = as.vector(r/imputed.pi*y+E.H%*%alpha.iid+ipw.beta%*%beta.pi.iid)
-  se.IPW = sqrt(var(mu.IPW.iid)/N)
-
-  mu.IPW.true.iid = as.vector(r/true.pi*y)
+  mu.IPW.true.iid = as.vector(r/true_ps*y)
   se.IPW.true = sqrt(var(mu.IPW.true.iid)/N)
   ################################################################################
 
@@ -234,36 +237,41 @@ EBMR_IPW = function(pi.fit.list, h, family = "gaussian", ortho = FALSE, true.pi 
               mu.IPW.true = mu.IPW.true,
               se.IPW = se.IPW,
               se.IPW.true = se.IPW.true,
-              pi.fit.list = pi.fit.list, candidate_ps = candidate_ps, dim.alpha = dim.alpha,
-              nu = nu.fit$nu.hat,
-              imbalance = sum(apply(nu.fit$g, 1, mean)^2),
+              ps_fit.list = ps_fit.list, ps.matrix = ps.matrix, alpha_dim = alpha_dim,
+              nu = ensemble_fit$nu.hat,
+              imbalance = sum(apply(ensemble_fit$g, 1, mean)^2),
               nu = nu))
 }
 
-EBMR_IPW_with_locally_misspecified_model = function(candidate_ps, perturb.index, exp.tilt, exp.tilt.x.names, h){
-  J = ncol(candidate_ps)
-  candidate_ps[, perturb.index] = exp.tilt(y[r == 1], dat[r == 1, exp.tilt.x.names])*candidate_ps[, perturb.index]
-  imputed.candidate_ps = matrix(1, N, J)
-  imputed.candidate_ps[r == 1, ] = candidate_ps
+EBMR_IPW_with_locally_misspecified_model = function(ps.matrix, perturb.index, exp.tilt, exp.tilt.x.names, h){
+  ################################################################################
+  # Collect the propensity score models
+  ################################################################################
+  J = ncol(ps.matrix)
+  ps.matrix[, perturb.index] = exp.tilt(y[r == 1], data[r == 1, exp.tilt.x.names])*ps.matrix[, perturb.index]
+  imputed.ps.matrix = matrix(1, N, J)
+  imputed.ps.matrix[r == 1, ] = ps.matrix
+  ################################################################################
+  
   ################################################################################
   # Compress the propensity score models
   ################################################################################
-  nu.fit = estimate_nu(h, candidate_ps, r, ortho = FALSE, init = rep(1/J, J))
-  nu = nu.fit$nu.hat
+  ensemble_fit = estimate_nu(h, ps.matrix, r, ortho = FALSE, init = rep(1/J, J))
+  nu = ensemble_fit$nu.hat
   nu = as.matrix((nu^2)/as.numeric(t(nu)%*%nu))
   compressed.pi = rep(1, N)
-  compressed.pi[r == 1] = candidate_ps%*%nu
+  compressed.pi[r == 1] = ps.matrix%*%nu
   ################################################################################
   
   ################################################################################
-  # calculate norm.nu.adj, beta.pi.iid ... (for estimating the ASE)
+  # calculate dot_W_nu_hat, nu.iid ... (for estimating the ASE)
   ################################################################################
-  norm.adj = function(w){
+  dot_W = function(w){
     (diag(2*w)*sum(w^2)-2*(w^2)%*%t(w))/(sum(w^2)^2)
   }
-  norm.nu.adj = 0; if(length(nu) > 1) norm.nu.adj = norm.adj(nu.fit$nu.hat)
+  dot_W_nu_hat = 0; if(length(nu) > 1) dot_W_nu_hat = dot_W(ensemble_fit$nu.hat)
   
-  beta.pi.iid = -nu.fit$K_nu%*%nu.fit$g
+  nu.iid = -ensemble_fit$K_nu%*%ensemble_fit$g
   ################################################################################
   
   ################################################################################
@@ -275,9 +283,9 @@ EBMR_IPW_with_locally_misspecified_model = function(candidate_ps, perturb.index,
   ################################################################################
   # Estimate the asymptotic variance of IPW
   ################################################################################
-  ipw.beta = apply(-r*y/(compressed.pi^2)*(imputed.candidate_ps%*%norm.nu.adj), 2, mean)
+  ipw.beta = apply(-r*y/(compressed.pi^2)*(imputed.ps.matrix%*%dot_W_nu_hat), 2, mean)
   
-  mu.IPW.iid = as.vector(r/compressed.pi*y+ipw.beta%*%beta.pi.iid)
+  mu.IPW.iid = as.vector(r/compressed.pi*y+ipw.beta%*%nu.iid)
   se.IPW = sqrt(var(mu.IPW.iid)/N)
   
   mu.IPW.true.iid = as.vector(r/compressed.pi*y)
@@ -286,8 +294,8 @@ EBMR_IPW_with_locally_misspecified_model = function(candidate_ps, perturb.index,
   
   return(list(mu.IPW = mu.IPW,
               se.IPW = se.IPW,
-              candidate_ps = candidate_ps,
-              nu = nu.fit$nu.hat,
+              ps.matrix = ps.matrix,
+              nu = ensemble_fit$nu.hat,
               nu = nu))
 }
 
