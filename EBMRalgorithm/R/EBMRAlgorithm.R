bd1f9016d0f4b0f6842ece3a5b89a36c1cde7f06
LukeChuang890212 <zhuang890212@gmail.com>
2025-03-04 01:55:13 +0800

    revise method.r

--- EBMRalgorithm/R/EBMRAlgorithm.R
+++ EBMRalgorithm/R/EBMRAlgorithm.R
@@ -86,6 +86,8 @@ EBMRAlgorithm <- R6Class("EBMRAlgorithm",
                      # public fields (variables)
                      WangShaoKim2014 = WangShaoKim2014,
                      EBMR_IPW = EBMR_IPW,
+                     WangShaoKim2014_perturb = WangShaoKim2014_perturb,
+                     EBMR_IPW_perturb = EBMR_IPW_perturb,
                      EBMR_IPW_with_locally_misspecified_model = EBMR_IPW_with_locally_misspecified_model
                    ),
 
@@ -101,101 +103,6 @@ EBMRAlgorithm <- R6Class("EBMRAlgorithm",
                      separate_variable_types = separate_variable_types,
                      estimate_nu = estimate_nu
                    )
-
-)
-
-#' EBMRAlgorithm Class
-#'
-#' The `EBMRAlgorithm_perturb` class provides an implementation for estimating
-#' the coefficients in a propensity score model and related functionalities
-#' based on the ensemble method for inverse probability weighting (IPW).
-#' This class allows users to perform propensity score fitting and estimation
-#' for the specified data and propensity score specifications.
-#'
-#' @import R6
-#'
-#' @section Public Methods:
-#' The following methods are available in the EBMRAlgorithm class:
-#'
-#' \code{initialize(y_names, ps_specifications, data)}:
-#' Initializes the class and estimates the propensity scores based on the provided formulas,
-#' covariates to be balanced, inverse link function, and the specified data.
-#'
-#' \code{WangShaoKim2014(formula, h_x_names, inv_link, init = NULL)}:
-#' Implements the Wang, Shao, and Kim (2014) approach for estimating propensity score models in the presence of missing-not-at-random (MNAR) data.
-#'
-#' \code{EBMR_IPW(h_x_names, true_ps = NULL)}:
-#' Computes the Inverse Probability Weighting (IPW) estimator for the population mean \eqn{\mu_0},
-#' using the propensity scores estimated by the ensemble method. It also computes standard errors and other related
-#' quantities for the estimator, including the estimator when the true propensity score is provided.
-#'
-#' @section Private Fields:
-#' \describe{
-#'   \item{\code{r}}{The response indicator.}
-#'   \item{\code{y}}{The outcome variable(s) used in the model.}
-#'   \item{\code{n}}{The number of observations.}
-#'   \item{\code{ps_fit.list}}{List of fitted propensity score models.}
-#' }
-#'
-#' @examples
-#' \dontrun{
-#' # Define propensity score specifications (for illustration)
-#' ps_specifications = list(
-#'   formula.list = list(formula1, formula2),
-#'   h_x_names.list = list(h_x1, h_x2),
-#'   inv_link = inv_link_function
-#' )
-#'
-#' # Initialize the EBMRAlgorithm class
-#' ebmr = EBMRAlgorithm$new(y_names = "outcome", ps_specifications = ps_specifications, data = dataset)
-#' }
-#'
-#' @export
-#'
-#' @name EBMRAlgorithm_perturb
-#'
-EBMRAlgorithm_perturb <- R6Class("EBMRAlgorithm_perturb",
-                         public = list(
-                           # Public fields (variables)
-                           data = NULL,
-                           ps_fit.list = list(),
-
-                           # Constructor to initialize fields
-                           initialize = function(y_names, ps_specifications, data) {
-                             self$data = private$check_data(y_names, data)
-                             private$r = self$data$r
-                             private$y = self$data[y_names]
-                             private$n = nrow(self$data)
-
-                             J = length(ps_specifications$formula.list)
-                             for(j in 1:J){
-                               formula = ps_specifications$formula.list[[j]]
-                               h_x_names = ps_specifications$h_x_names.list[[j]]
-                               inv_link = ps_specifications$inv_link
-                               self$ps_fit.list[[j]] = self$WangShaoKim2014(formula, h_x_names, inv_link)
-                             }
-                             # print(self$ps_fit.list[[3]]$coefficients)
-                             # print(self$ps_fit.list[[3]]$se)
-                           },
-
-                           # public fields (variables)
-                           WangShaoKim2014_perturb = WangShaoKim2014_perturb,
-                           EBMR_IPW_perturb = EBMR_IPW_perturb,
-                           EBMR_IPW_with_locally_misspecified_model = EBMR_IPW_with_locally_misspecified_model
-                         ),
-
-                         private = list(
-                           # private fields (variables)
-                           r = NULL,
-                           y = NULL,
-                           n = NULL,
-
-                           # private methods
-                           check_data = check_data,
-                           parse_formula = parse_formula,
-                           separate_variable_types = separate_variable_types,
-                           estimate_nu = estimate_nu
-                         )
 
 )
 
